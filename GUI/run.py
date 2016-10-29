347e911dddaded4a8c279baf956a35e3180a37f9
Sergej Kuzmin <serhiy.kuzmin@haw-hamburg.de>
2016-10-29 02:03:41 +0200

    UI to PY converted.

--- GUI/run.py
+++ GUI/run.py
@@ -1,23 +1,14 @@
-import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys 
+import Materialtheorie
 
-
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
- 
-ui_file_path =  "Materialtheorie.ui"
-
-class MyApp(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-
-        # Parent class init
-        QtGui.QMainWindow.__init__(self) 
-        # You may also write
-        # super(MyApp, self).__init__()
-
-        # Load UI
-        uic.loadUi(ui_file_path, self)
-
+class MainWin(QtWidgets.QMainWindow,Materialtheorie.Ui_xx):
+      def __init__(self, parent = None):
+            super(Main.UiClass,self).__init__(parent)
+            self.setupUi(self)
+            
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    window = MyApp()
-    window.show()
+    app = QtWidgets.QApplication(sys.argv)
+    xx = QtWidgets.QMainWindow()
+    xx.show()
     sys.exit(app.exec_())
